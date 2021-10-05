e3fd8222f59fda75ab41bd82eccd03c6d8445712
GustavoMori <gustavomoripeixoto@hotmail.com>
2021-10-05 14:24:29 -0300

    fixing test of candidates_skill's model

--- spec/models/candidates_skill_spec.rb
+++ spec/models/candidates_skill_spec.rb
@@ -2,18 +2,20 @@ describe CandidatesSkill do
   describe 'Association with skills' do
     it 'Can be associated with skills' do
       skill = Skill.create(name: 'ruby')
-      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br', skills: [skill])
+      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br')
+      candidates_skill = CandidatesSkill.create(skill_id: skill.id, candidate_id: candidate.id)
 
-      expect(candidate.skills).to eq([skill])
+      expect(candidates_skill.skill).to eq(skill)
     end
   end
-  
+
   describe 'Association with candidate' do
     it 'Can be associated with candidates' do
       skill = Skill.create(name: 'ruby')
-      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br', skills: [skill])
+      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br')
+      candidates_skill = CandidatesSkill.create(skill_id: skill.id, candidate_id: candidate.id)
 
-      expect(skill.candidates).to eq([candidate])
+      expect(candidates_skill.candidate).to eq(candidate)
     end
   end
 end
