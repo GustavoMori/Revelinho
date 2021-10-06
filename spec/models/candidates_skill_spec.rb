f98c34c5bca75e76181b0604293b862096e26601
GustavoMori <gustavomoripeixoto@hotmail.com>
2021-10-06 10:06:19 -0300

    implementation if is possible create a candidate_skill without relationship

--- spec/models/candidates_skill_spec.rb
+++ spec/models/candidates_skill_spec.rb
@@ -18,4 +18,20 @@ describe CandidatesSkill do
       expect(candidates_skill.candidate).to eq(candidate)
     end
   end
+
+  describe 'When a candidate_skill is created without relationship of candidate or skill' do
+    it 'Verify if is persisted without relationship of skill it fails' do
+      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br')
+      candidates_skill = CandidatesSkill.create(skill_id: nil, candidate_id: candidate.id)
+
+      expect(candidates_skill).not_to be_persisted
+    end
+
+    it 'Verify if is persisted without relationship of candidate it fails' do
+      skill = Skill.create(name: 'ruby')
+      candidates_skill = CandidatesSkill.create(skill_id: skill.id, candidate_id: nil)
+
+      expect(candidates_skill).not_to be_persisted
+    end
+  end
 end
