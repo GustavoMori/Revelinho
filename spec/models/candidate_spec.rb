93dbe97cea39ec775fc0bdfb83ed71b804dc7215
GustavoMori <gustavomoripeixoto@hotmail.com>
2021-10-07 13:48:56 -0300

    use of subject

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -1,52 +1,57 @@
 describe Candidate do
+
+  subject { described_class.new(name: 'Oko', email: 'oko.oko@revelo.com.br') }
+
   describe 'When create a candidate, validate the emails is obligated' do
     it 'return true if email is valid' do
-      candidate = Candidate.new(name: 'Oko', email: 'oko.oko@revelo.com.br')
-      expect(candidate).to be_valid
+      expect(subject).to be_valid
     end
 
     it 'return false if email is invalid' do
-      candidate = Candidate.new name: 'Oko'
-      expect(candidate).not_to be_valid
+      subject.email = nil
+      expect(subject).not_to be_valid
     end
   end
 
   describe '#skills' do
     it 'Can be associated with skills' do
       skill = Skill.create(name: 'ruby')
-      candidate = Candidate.new(name: 'Oko', email: 'oko.oko@revelo.com.br', skills: [skill])
+      subject.skills = [skill]
 
-      expect(candidate.skills).to eq([skill])
+      expect(subject.skills).to eq([skill])
     end
   end
 
   describe '#candidates_skills' do
     it 'Can associate candidates and skill' do
-      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br')
+      subject.save
       skill = Skill.create(name: 'ruby')
-      candidates_skills = CandidatesSkill.create(candidate_id: candidate.id, skill: skill)
+      candidates_skills = CandidatesSkill.create(candidate_id: subject.id, skill: skill)
       
-      expect(candidate.candidates_skills).to eq([candidates_skills])
+      expect(subject.candidates_skills).to eq([candidates_skills])
     end
   end
 
   describe '#careers' do
     it 'verify that fullstack is a valid career' do
-      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br', careers: 'fullstack')
-      
-      expect(candidate.careers).to eq('fullstack')
+      subject.careers = 'fullstack'
+      subject.save!
+
+      expect(subject.careers).to eq('fullstack')
     end
 
     it 'verify that frontend is a valid career' do
-      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br', careers: 'frontend')
-      
-      expect(candidate.careers).to eq('frontend')
+      subject.careers = 'frontend'
+      subject.save!
+
+      expect(subject.careers).to eq('frontend')
     end
 
     it 'verify that backend is a valid career' do
-      candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br', careers: 'backend')
-      
-      expect(candidate.careers).to eq('backend')
+      subject.careers = 'backend'
+      subject.save!
+
+      expect(subject.careers).to eq('backend')
     end
   end
 end
