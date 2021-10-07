8a827bd5b917256f0cc7fb9b194730e588aeb4e8
GustavoMori <gustavomoripeixoto@hotmail.com>
2021-10-07 13:49:52 -0300

    fix of test skill_serializer

--- spec/serializers/skill_serializer_spec.rb
+++ spec/serializers/skill_serializer_spec.rb
@@ -1,7 +1,8 @@
 describe SkillSerializer do
   describe '#serializable_hash' do
     it 'return a skill serialized' do
-      javascript = Skill.create(name: 'javascript', id: 2321313231)
+      javascript = Skill.create(name: 'javascript')
+      
       serializer = SkillSerializer.new javascript
 
       expect(serializer.serializable_hash).to eq({
