d60eff3d744e5f48576ea2a0bbc34cb55f45cf17
GustavoMori <gustavomoripeixoto@hotmail.com>
2021-10-08 09:33:26 -0300

    lint fix

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,7 +1,7 @@
 class SkillsController < ApplicationController
   def index
     #render json: Skill.all()
-    skill = Skill.all()
+    skill = Skill.all
     render json: SkillSerializer.new(skill)
   end
 end
