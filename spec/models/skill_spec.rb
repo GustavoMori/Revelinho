d99916895e669c83a68ea539abcce020ff7169f9
GustavoMori <gustavomoripeixoto@hotmail.com>
2021-10-14 16:29:34 -0300

    use a factory skill in models test

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -3,7 +3,7 @@
 describe Skill do
   describe '#candidates' do
     it 'Can be associated with candidates' do
-      skill = Skill.create(name: 'ruby')
+      skill = create(:skill)
       candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br', skills: [skill])
 
       expect(skill.candidates).to eq([candidate])
@@ -13,7 +13,7 @@ describe Skill do
   describe '#candidates_skills' do
     it 'Can associate skill and candidates' do
       candidate = Candidate.create(name: 'Oko', email: 'oko.oko@revelo.com.br')
-      skill = Skill.create(name: 'ruby')
+      skill = create(:skill)
       candidates_skills = CandidatesSkill.create(candidate_id: candidate.id, skill: skill)
 
       expect(skill.candidates_skills).to eq([candidates_skills])
@@ -23,23 +23,23 @@ describe Skill do
   describe 'Names validations' do
     context 'When create a skill without name' do
       it 'is invalid' do
-        skill = Skill.create(name: nil)
+        skill = build(:skill, name: nil)
         expect(skill).not_to be_valid
       end
     end
 
     it 'allow ruby as a name' do
-      skill = Skill.create(name: 'ruby')
+      skill = build(:skill, name: 'ruby')
       expect(skill).to be_valid
     end
 
     it 'allow rails as a name' do
-      skill = Skill.create(name: 'rails')
+      skill = build(:skill, name: 'rails')
       expect(skill).to be_valid
     end
 
     it 'allow javascript as a name' do
-      skill = Skill.create(name: 'javascript')
+      skill = build(:skill, name: 'javascript')
       expect(skill).to be_valid
     end
   end
